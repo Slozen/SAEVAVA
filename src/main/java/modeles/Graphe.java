fea316a4996a953e4517f2b43025e2ebc238aef3
Dorlanbenz <ratovo.orlando@gmail.com>
2025-05-28 17:25:27 +0200

    Add files via upload
    modif graphe + tri topo

--- src/main/java/modeles/Graphe.java
+++ src/main/java/modeles/Graphe.java
@@ -1,84 +1,121 @@
 package modeles;
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Graphe {
     private TreeMap<String, Set<String>> sommetVoisin;
-    private TreeMap<String, String> degreEntrant;
+    private TreeMap<String, Integer> degreEntrant;
+    public static final String VELIZY_DEPART = "Velizy+"; //departVelizy
+    public static final String VELIZY_RETOUR = "Velizy-"; //arriveeVelizy
 
     public Graphe(ArrayList<Livraison> livraisons) {
         sommetVoisin = new TreeMap<>();
         degreEntrant = new TreeMap<>();
+
+        Set<String> villesPlus = new HashSet<>();
+        Set<String> villesMoins = new HashSet<>();
+
+        // Ajouter les arcs vendeur → acheteur
+        for (Livraison livraison : livraisons) {
+            String vendeur = livraison.getVilleDepart() + "+";
+            String acheteur = livraison.getVilleArrivee() + "-";
+
+            villesPlus.add(vendeur);
+            villesMoins.add(acheteur);
+
+            if (!sommetVoisin.containsKey(vendeur)) {
+                sommetVoisin.put(vendeur, new TreeSet<>());
+            }
+            if (!sommetVoisin.containsKey(vendeur)) {
+                sommetVoisin.put(acheteur, new TreeSet<>());
+            }
+
+            sommetVoisin.get(vendeur).add(acheteur);
+        }
+
+        // initialiser tous les degrés entrants à 0
+        for (String sommet : sommetVoisin.keySet()) {
+            degreEntrant.put(sommet, 0);
+        }
+
+        // calcul des degrés entrants
+        for (String sommet : sommetVoisin.keySet()) {
+            for (String voisin : sommetVoisin.get(sommet)) {
+                int deg = degreEntrant.getOrDefault(voisin, 0);
+                degreEntrant.put(voisin, deg + 1);
+            }
+        }
+
+        sommetVoisin.put(VELIZY_DEPART, new TreeSet<>());
+        for (String villePlus : villesPlus) {
+            sommetVoisin.get(VELIZY_DEPART).add(villePlus);
+            degreEntrant.put(villePlus, degreEntrant.get(villePlus) + 1);
+        }
+        degreEntrant.put(VELIZY_DEPART, 0);
+
+        sommetVoisin.put(VELIZY_RETOUR, new TreeSet<>());
+        for (String villeMoins : villesMoins) {
+            if (!sommetVoisin.containsKey(villeMoins)) {
+                sommetVoisin.put(villeMoins, new TreeSet<>());
+            }
+            sommetVoisin.get(villeMoins).add(VELIZY_RETOUR);
+            degreEntrant.put(VELIZY_RETOUR, degreEntrant.getOrDefault(VELIZY_RETOUR, 0) + 1);
+        }
+        degreEntrant.putIfAbsent(VELIZY_RETOUR, 0);
+
     }
 
-    public Set getSommets(){
+    public Set<String> getSommets() {
         return sommetVoisin.keySet();
     }
 
-    public int getOrdre(){
+    public int getOrdre() {
         return sommetVoisin.size();
     }
 
-    public int degre(int sommet){
+    public int degre(String sommet) {
         return sommetVoisin.get(sommet).size();
     }
 
-    public int taille(){
-        int somme = 0;
-        for(int i : sommetVoisin.keySet()) {
-            somme += this.degre(i);
+    public int taille() {
+        int total = 0;
+        for (String s : sommetVoisin.keySet()) {
+            total += this.degre(s);
         }
-        return somme /2;
+        return total;
     }
 
-    public int degre_minimal(){
-        int[] tousDegreMin = new int[this.getOrdre()];
-        int ind = 0;
-        for(int i : sommetVoisin.keySet()) {
-            tousDegreMin[ind] = this.degre(i);
-            ind++;
-
-        }
-        int min = tousDegreMin[0];
-        for(int j : tousDegreMin) {
-            if(j < min) {
-                min = j;
+    public ArrayList<String> degreEntrantZero(){
+        ArrayList<String> degresEntrantZero = new ArrayList<>();
+        for (String sommet : sommetVoisin.keySet()){
+            if(degreEntrant.get(sommet).equals(0)){
+                degresEntrantZero.add(sommet);
             }
         }
-        return min;
+        return degresEntrantZero;
     }
 
-    public int degre_maximal(){
-        int[] tousDegreMax = new int[this.getOrdre()];
-        int ind = 0;
-        for(int i : sommetVoisin.keySet()) {
-            tousDegreMax[ind] = this.degre(i);
-            ind++;
-        }
-        int max = tousDegreMax[0];
-        for(int j : tousDegreMax) {
-            if(j > max) {
-                max = j;
+    public ArrayList<String> triToploogique(){
+        ArrayList<String> sources = degreEntrantZero();
+        ArrayList<String> resultat = new ArrayList<>();
+        while (!sources.isEmpty()){
+            String source = sources.remove(0);
+            resultat.add(source);
+            for (String voisin : sommetVoisin.get(source)){
+                degreEntrant.put(voisin, degreEntrant.get(voisin)-1);
+                if (degreEntrant.get(voisin) == 0){
+                    sources.add(voisin);
+                }
             }
         }
-        return max;
-
+        return resultat;
     }
 
-    /*public ArrayList<Integer> topologie(){
-        ArrayList<Integer> listeTopo = new ArrayList<>();
-        TreeMap<Integer, Integer> degEntrant = degreEntrant;
-
-    }*/
 
     public String toString() {
-        String affichage = "ordre : " + this.getOrdre() + "\n" + "taille : " + this.taille() + "\n"
-                + "degré_minimal : " + this.degre_minimal() + "\n" + "degré_maximal : " + this.degre_maximal() + "\n" + degreEntrant.toString() + "\n";
-        for(int i : sommetVoisin.keySet()) {
-            affichage += "sommet " + i + " degré=" + this.degre(i) + " voisins : " + sommetVoisin.get(i) + "\n";
+        String affichage = "ordre : " + this.getOrdre() + "\n" + "taille : " + this.taille() + "\n" + "degré entrant : " + degreEntrant.toString() + "\n";
+        for (String i : sommetVoisin.keySet()) {
+            affichage += "sommet " + i + ", degré sortant = " + this.degre(i) + " voisins : " + sommetVoisin.get(i) + "\n";
         }
         return affichage;
     }
